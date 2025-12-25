385b51adf5731e7ee09d89372229135372e5b0c2
ondrejkyncl <ondrej.kyncl.025@pslib.cz>
2025-12-26 00:15:44 +0100

    session 2: ball physics

--- logic/main.js
+++ logic/main.js
@@ -1,6 +1,19 @@
+
+Object.defineProperty(exports, "__esModule", { value: true });
 var gameCanvas = document.getElementById('pongCanvas');
 var ctx = gameCanvas.getContext('2d');
+var ballX = 425;
+var ballY = 300;
+var ballspeedX = 2;
+var ballspeedY = 2;
+//we create a function that makes a constant loop of updates
+function gameLoop() {
+    ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+    ballX = ballX + ballspeedX;
+    ballY = ballY + ballspeedY;
+    ctx.fillRect(ballX, ballY, 15, 15);
+    ctx.fillRect(50, 250, 10, 100);
+    ctx.fillRect(790, 250, 10, 100);
+    requestAnimationFrame(gameLoop);
+}
 ctx.fillStyle = '#ffffff';
-ctx.fillRect(415, 300, 15, 15);
-ctx.fillRect(50, 250, 10, 100);
-ctx.fillRect(790, 250, 10, 100);
--- logic/main.ts
+++ logic/main.ts
@@ -1,7 +1,19 @@
 export {};
 const gameCanvas = document.getElementById('pongCanvas') as HTMLCanvasElement;
 const ctx = gameCanvas.getContext('2d')!
+let ballX: number = 425
+let ballY: number = 300
+let ballspeedX: number = 2
+let ballspeedY: number = 2
+
+//we create a function that makes a constant loop of updates
+function gameLoop () {
+    ctx.clearRect(0,0, gameCanvas.width, gameCanvas.height);
+    ballX = ballX + ballspeedX
+    ballY = ballY + ballspeedY
+    ctx.fillRect(ballX, ballY, 15, 15);
+    ctx.fillRect(50, 250, 10, 100);
+    ctx.fillRect(790, 250, 10, 100);
+    requestAnimationFrame(gameLoop);
+}
 ctx.fillStyle = '#ffffff'
-ctx.fillRect(415, 300, 15, 15);
-ctx.fillRect(50, 250, 10, 100);
-ctx.fillRect(790, 250, 10, 100);
