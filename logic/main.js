424409ce00d57ada05b9523a3e57bf48e6e0d5d9
ondrejkyncl <ondrej.kyncl.025@pslib.cz>
2025-12-25 19:25:56 +0100

    session 1: graphics, fix

---
+++ logic/main.js
@@ -0,0 +1,6 @@
+var gameCanvas = document.getElementById('pongCanvas');
+var ctx = gameCanvas.getContext('2d');
+ctx.fillStyle = '#ffffff';
+ctx.fillRect(415, 300, 15, 15);
+ctx.fillRect(50, 250, 10, 100);
+ctx.fillRect(790, 250, 10, 100);
--- logic/main.ts
+++ logic/main.ts
@@ -1,3 +1,7 @@
-const canvas = document.getElementById('pongCanvas') as HTMLCanvasElement;
-const context = canvas.getContext('2d')!
-context.fillStyle{#ffffff}
+export {};
+const gameCanvas = document.getElementById('pongCanvas') as HTMLCanvasElement;
+const ctx = gameCanvas.getContext('2d')!
+ctx.fillStyle = '#ffffff'
+ctx.fillRect(415, 300, 15, 15);
+ctx.fillRect(50, 250, 10, 100);
+ctx.fillRect(790, 250, 10, 100);
