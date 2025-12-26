eb6d4d1d3222111c9cf34c1b4d2140db43bacb87
ondrejkyncl <ondrej.kyncl.025@pslib.cz>
2025-12-26 19:27:59 +0100

    session 3: Paddles, AI, Ball x Paddle collisions, controls

--- logic/main.js
+++ logic/main.js
@@ -5,6 +5,12 @@ var ballY = 300;
 var ballspeedX = 2;
 var ballspeedY = 2;
 var radius = 7.5; //half of 15, since 15 is the diameter of the cube
+var player1Y = 250;
+var player2Y = 250;
+var player1UP = false;
+var player1DOWN = false;
+var wKeyIsPressed = false;
+var sKeyIsPressed = false;
 //we create a function that makes a constant loop of updates
 function gameLoop() {
     ctx.fillStyle = '#ffffff';
@@ -17,9 +23,34 @@ function gameLoop() {
     if (ballX - radius < 0 || ballX + radius > gameCanvas.width) {
         ballspeedX = ballspeedX * -1;
     }
+    if (wKeyIsPressed && player1Y >= 0) {
+        player1Y = player1Y - 5;
+    }
+    if (sKeyIsPressed && player1Y >= 0) {
+        player1Y = player1Y + 5;
+    }
+    if (sKeyIsPressed && player1Y >= 500) {
+        player1Y = 500;
+    }
     ctx.fillRect(ballX - 7.5, ballY - 7.5, 15, 15); //ballX and ballY are the CENTER of the ball, which has the radius of 15x15px
-    ctx.fillRect(50, 250, 10, 100);
-    ctx.fillRect(790, 250, 10, 100);
+    ctx.fillRect(50, player1Y, 10, 100);
+    ctx.fillRect(790, player2Y, 10, 100);
     requestAnimationFrame(gameLoop);
 }
 gameLoop();
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'w' || event.key === 'W') {
+        wKeyIsPressed = true;
+    }
+    if (event.key === 's' || event.key === 'S') {
+        sKeyIsPressed = true;
+    }
+});
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'w' || event.key === 'W') {
+        wKeyIsPressed = false;
+    }
+    if (event.key === 's' || event.key === 'S') {
+        sKeyIsPressed = false;
+    }
+});
--- logic/main.ts
+++ logic/main.ts
@@ -1,4 +1,4 @@
-export {}
+
 const gameCanvas = document.getElementById('pongCanvas') as HTMLCanvasElement;
 const ctx = gameCanvas.getContext('2d')!
 let ballX: number = 425
@@ -6,6 +6,12 @@ let ballY: number = 300
 let ballspeedX: number = 2
 let ballspeedY: number = 2
 let radius: number = 7.5 //half of 15, since 15 is the diameter of the cube
+let player1Y: number = 250
+let player2Y: number = 250
+let player1UP: boolean = false
+let player1DOWN: boolean = false
+let wKeyIsPressed: boolean = false
+let sKeyIsPressed: boolean = false
 
 //we create a function that makes a constant loop of updates
 function gameLoop () {
@@ -19,10 +25,37 @@ function gameLoop () {
         if (ballX - radius < 0 || ballX + radius > gameCanvas.width) {
             ballspeedX = ballspeedX * -1;
         }
+        if (wKeyIsPressed && player1Y >= 0) {
+            player1Y = player1Y - 5
+        }
+        if (sKeyIsPressed && player1Y >= 0) {
+            player1Y = player1Y + 5
+        }
+        if (sKeyIsPressed && player1Y >= 500) {
+        player1Y = 500
+    }
     ctx.fillRect(ballX -7.5, ballY -7.5, 15, 15); //ballX and ballY are the CENTER of the ball, which has the radius of 15x15px
-    ctx.fillRect(50, 250, 10, 100);
-    ctx.fillRect(790, 250, 10, 100);
+    ctx.fillRect(50, player1Y, 10, 100);
+    ctx.fillRect(790, player2Y, 10, 100);
     requestAnimationFrame(gameLoop);
     
 }
 gameLoop();
+
+document.addEventListener ('keydown', (event) => {
+    if (event.key === 'w' || event.key === 'W') {
+        wKeyIsPressed = true;
+    }
+    if (event.key === 's' || event.key === 'S') {
+        sKeyIsPressed = true;
+    }
+});
+
+document.addEventListener ('keyup', (event) => {
+    if (event.key === 'w' || event.key === 'W') {
+        wKeyIsPressed = false;
+    }
+    if (event.key === 's' || event.key === 'S') {
+        sKeyIsPressed = false;
+    }
+});
